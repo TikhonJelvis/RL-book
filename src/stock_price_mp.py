988224c3d2d7c2b72e14f69e575413f3635ef516
Tikhon Jelvis <tikhon.jelvis@target.com>
2020-05-30 09:39:10 -0700

    Wrote a dataclass version of Model2.

--- src/stock_price_mp.py
+++ src/stock_price_mp.py
@@ -1,16 +1,39 @@
+from dataclasses import dataclass
 from typing import Tuple, Optional, Mapping, List
 from numpy import exp
 from numpy.random import binomial
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
 level_param: int = 100
 pull_param: float = 0.7  # pull_param should be in the interval [0., 1.]
 
-handy_map: Mapping[Optional[bool], int] = {
-    True: -1,
-    False: 1,
-    None: 0
-}
+handy_map: Mapping[Optional[bool], int] = {True: -1, False: 1, None: 0}
+
+
+@dataclass
+class Model2:
+    @dataclass
+    class State:
+        price: int
+        previous_direction: Optional[bool]
+
+    pull_param: float = 0.7
+
+    def up_prob(self, state: State):
+        return 0.5 * (1 + pull_param * handy_map[state.previous_direction])
+
+    def next_state(self, state: State) -> State:
+        up_move: int = binomial(1, self.up_prob(state), 1)[0]
+        return Model2.State(price=state.price + up_move * 2 - 1,
+                            previous_direction=bool(up_move))
+
+
+def simulation(model, start_state):
+    state = start_state
+    while True:
+        next_state = model.next_state(state)
+        state = next_state
+        yield next_state
 
 
 def model1_up_prob(state: Tuple[int]) -> float:
@@ -30,43 +53,46 @@ def model1_sample_next_state(state: Tuple[int]) -> Tuple[int]:
     return state[0] + up_move * 2 - 1,
 
 
-def model2_sample_next_state(state: Tuple[int, Optional[bool]]) -> Tuple[int, bool]:
+def model2_sample_next_state(
+        state: Tuple[int, Optional[bool]]) -> Tuple[int, bool]:
     up_move: int = binomial(1, model2_up_prob(state), 1)[0]
     return state[0] + up_move * 2 - 1, bool(up_move)
 
 
-def model3_sample_next_state(state: Tuple[int, int, int]) -> Tuple[int, int, int]:
+def model3_sample_next_state(
+        state: Tuple[int, int, int]) -> Tuple[int, int, int]:
     up_move: int = binomial(1, model3_up_prob(state), 1)[0]
-    return state[0] + up_move * 2 - 1, state[1] + up_move, state[2] + 1 - up_move
+    return state[0] + up_move * 2 - 1, state[1] + up_move, state[
+        2] + 1 - up_move
 
 
-if __name__ == '__main__':
-    start_price: int = 100
-    steps = 100
+# if __name__ == '__main__':
+#     start_price: int = 100
+#     steps = 100
 
-    model1_states: List[Tuple[int]] = [(start_price,)]
-    model2_states: List[Tuple[int, Optional[bool]]] = [(start_price, None)]
-    model3_states: List[Tuple[int, int, int]] = [(start_price, 0, 0)]
+#     model1_states: List[Tuple[int]] = [(start_price,)]
+#     model2_states: List[Tuple[int, Optional[bool]]] = [(start_price, None)]
+#     model3_states: List[Tuple[int, int, int]] = [(start_price, 0, 0)]
 
-    for _ in range(steps):
-        model1_states.append(model1_sample_next_state(model1_states[-1]))
-        model2_states.append(model2_sample_next_state(model2_states[-1]))
-        model3_states.append(model3_sample_next_state(model3_states[-1]))
+#     for _ in range(steps):
+#         model1_states.append(model1_sample_next_state(model1_states[-1]))
+#         model2_states.append(model2_sample_next_state(model2_states[-1]))
+#         model3_states.append(model3_sample_next_state(model3_states[-1]))
 
-    x_vals = range(steps + 1)
-    model1_y_vals = [x[0] for x in model1_states]
-    model2_y_vals = [x[0] for x in model2_states]
-    model3_y_vals = [x[0] for x in model3_states]
-    y_min = min(min(model1_y_vals), min(model2_y_vals), min(model3_y_vals))
-    y_max = max(max(model1_y_vals), max(model2_y_vals), max(model3_y_vals))
-    plt.figure(figsize=(12, 8))
-    plt.plot(x_vals, model1_y_vals, "r", label="Based on current price")
-    plt.plot(x_vals, model2_y_vals, "b", label="Based on previous move")
-    plt.plot(x_vals, model3_y_vals, "g", label="Based on entire history")
-    plt.xlabel("Time Steps", fontsize=20)
-    plt.ylabel("Stock Price", fontsize=20)
-    plt.title("Simulation for 3 Models", fontsize=25)
-    plt.axis((0, steps, y_min, y_max))
-    plt.grid(True)
-    plt.legend(fontsize=15)
-    plt.show()
+#     x_vals = range(steps + 1)
+#     model1_y_vals = [x[0] for x in model1_states]
+#     model2_y_vals = [x[0] for x in model2_states]
+#     model3_y_vals = [x[0] for x in model3_states]
+#     y_min = min(min(model1_y_vals), min(model2_y_vals), min(model3_y_vals))
+#     y_max = max(max(model1_y_vals), max(model2_y_vals), max(model3_y_vals))
+#     plt.figure(figsize=(12, 8))
+#     plt.plot(x_vals, model1_y_vals, "r", label="Based on current price")
+#     plt.plot(x_vals, model2_y_vals, "b", label="Based on previous move")
+#     plt.plot(x_vals, model3_y_vals, "g", label="Based on entire history")
+#     plt.xlabel("Time Steps", fontsize=20)
+#     plt.ylabel("Stock Price", fontsize=20)
+#     plt.title("Simulation for 3 Models", fontsize=25)
+#     plt.axis((0, steps, y_min, y_max))
+#     plt.grid(True)
+#     plt.legend(fontsize=15)
+#     plt.show()
