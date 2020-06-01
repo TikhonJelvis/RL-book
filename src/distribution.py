24cbce5b57ab0f6365d27b31d041556135329dfc
Tikhon Jelvis <tikhon.jelvis@target.com>
2020-06-01 12:00:43 -0700

    Base classes for distributions, with two example distributions.
    Defined a couple of classes that basically serve as interfaces that can represent any kind of distribution. The idea is that we can have two kinds of distributions:
    
      1. Arbitrary distributions where all we can do is sample.
      2. Finite distributions that we can render as a PDF or CDF table.
    
    This is captured in two abstract base classes: `Distribution` for 1 and `FiniteDistribution` for 2. Continuous distributions—which I have not implemented yet—would be subclasses of `Distribution` but *not* `FiniteDistribution`.
    
    Having classes like this gives us a lot of flexibility: we have an explicit `Distribution` type to use in our process classes (MP, MRP, MDP... etc) and we can implement a lot of *different* distributions using different technologies under the hood (ie our own logic vs calling numpy vs whatever else) in a way that hides those details and lets us use different implementations totally interchangeably in the rest of our code.
    
    This is a key software design decision that we can talk about in the book: by using interfaces, we ensure that our process code and algorithms only need to specify what functionality they *need* from a distribution (ie tabular vs sampling), without needing to know exactly how the distribution is computed. This cleanly separates the concerns between different parts of the code and makes it easy to add both new sampling algorithms *and* new optimization algorithms without either needing to know about the other.
    
    To illustrate how this works, I added two simple example distributions:
    
      1. A `Bernoulli` distribution that always returns booleans.
      2. A `Choose` distribution that choose an outcome from a list uniformly at random.
    
    As we go along, we can define more distributions in this style.

--- src/distribution.py
+++ src/distribution.py
@@ -1,1 +1,61 @@
-class Distribution:
+from abc import ABC, abstractmethod
+import random
+from typing import Generic, List, Tuple, TypeVar
+
+A = TypeVar('A')
+
+
+class Distribution(ABC, Generic[A]):
+    '''A probability distribution that we can sample.
+
+    '''
+    @abstractmethod
+    def sample(self) -> A:
+        '''Return a random sample from this distribution.
+
+        '''
+        pass
+
+
+class FiniteDistribution(Distribution[A], ABC):
+    '''A probability distribution with a finite number of outcomes, which
+    means we can render it as a PDF or CDF table.
+
+    '''
+    @abstractmethod
+    def to_pdf(self) -> List[Tuple[A, float]]:
+        '''Returns a tabular representaiton of the probability density
+        function (PDF) for this distribution.
+
+        '''
+        pass
+
+
+class Bernoulli(FiniteDistribution[bool]):
+    '''A distribution with two outcomes. Returns True with probability p
+    and False with probability 1 - p.
+
+    '''
+    def __init__(self, p: float):
+        self.p = p
+
+    def sample(self) -> bool:
+        return random.uniform(0, 1) < self.p
+
+    def to_pdf(self) -> List[Tuple[bool, float]]:
+        return [(True, self.p), (False, 1 - self.p)]
+
+
+class Choose(FiniteDistribution[A]):
+    '''Select an element of the given list uniformly at random.
+
+    '''
+    def __init__(self, options: List[A]):
+        self.options = options
+
+    def sample(self) -> A:
+        return self.options[random.randrange(len(self.options))]
+
+    def to_pdf(self) -> List[Tuple[A, float]]:
+        length = len(self.options)
+        return [(x, 1.0 / length) for x in self.options]
