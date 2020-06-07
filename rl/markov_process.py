5ab471071ba1bbc134a71a59fe5fcbb67d7404c3
Ashwin Rao <ashwin.rao@gmail.com>
2020-06-06 21:32:11 -0700

    constructors no longer need start_state and also resolved multiple inhertiance problem

--- rl/markov_process.py
+++ rl/markov_process.py
@@ -13,19 +13,14 @@ class MarkovProcess(ABC, Generic[S]):
 
     '''
 
-    state: S
-
-    def __init__(self, start_state: S):
-        self.state = start_state
-
     @abstractmethod
-    def transition(self) -> Distribution[S]:
-        '''Given the current state of the process, returns a distribution of
+    def transition(self, state: S) -> Distribution[S]:
+        '''Given a state of the process, returns a distribution of
         the next states.
 
         '''
 
-    def simulate(self) -> Iterable[S]:
+    def simulate(self, start_state: S) -> Iterable[S]:
         '''Run a simulation trace of this Markov process, generating the
         states visited during the trace.
 
@@ -34,9 +29,10 @@ class MarkovProcess(ABC, Generic[S]):
 
         '''
 
+        state: S = start_state
         while True:
-            yield self.state
-            self.state = self.transition().sample()
+            yield state
+            state = self.transition(state).sample()
 
 
 class FiniteMarkovProcess(MarkovProcess[S]):
@@ -53,12 +49,9 @@ class FiniteMarkovProcess(MarkovProcess[S]):
 
     def __init__(
             self,
-            start_state: S,
-            state_space: Sequence[S],
             transition_map: S_TransType
     ):
-        super().__init__(start_state)
-        self.state_space = state_space
+        self.state_space = list(transition_map.keys())
         self.transition_map = transition_map
         self.transition_matrix = self.get_transition_matrix()
 
@@ -70,8 +63,8 @@ class FiniteMarkovProcess(MarkovProcess[S]):
                 mat[i, j] = self.transition_map[s1].get(s2, 0.)
         return mat
 
-    def transition(self) -> FiniteDistribution[S]:
-        return Categorical(self.transition_map[self.state].items())
+    def transition(self, state: S) -> FiniteDistribution[S]:
+        return Categorical(self.transition_map[state].items())
 
     def get_stationary_distribution(self) -> FiniteDistribution[S]:
         eig_vals, eig_vecs = np.linalg.eig(self.transition_matrix.T)
@@ -87,37 +80,38 @@ class FiniteMarkovProcess(MarkovProcess[S]):
 
 
 class MarkovRewardProcess(MarkovProcess[S]):
-    def transition(self) -> Distribution[S]:
+    def transition(self, state: S) -> Distribution[S]:
         '''Transitions the Markov Reward Process, ignoring the generated
         reward (which makes this just a normal Markov Process).
 
         '''
-        def next_state():
-            state, _ = self.transition_reward().sample()
-            return state
+        def next_state(state=state):
+            next_s, _ = self.transition_reward(state).sample()
+            return next_s
 
         return SampledDistribution(next_state)
 
     @abstractmethod
-    def transition_reward(self) -> Distribution[Tuple[S, float]]:
-        '''Given the current state, returns a distribution of the next state
+    def transition_reward(self, state: S) -> Distribution[Tuple[S, float]]:
+        '''Given a state, returns a distribution of the next state
         and reward from transitioning between the states.
 
         '''
 
-    def simulate_reward(self) -> Iterable[Tuple[S, float]]:
+    def simulate_reward(self, start_state: S) -> Iterable[Tuple[S, float]]:
         '''Simulate the MRP, yielding the new state and reward for each
         transition.
 
         The trace starts with the start state and a reward of 0.
 
         '''
-        yield self.state, 0
+
+        state: S = start_state
+        reward: float = 0.
 
         while True:
-            next_state, reward = self.transition_reward().sample()
-            self.state = next_state
-            yield next_state, reward
+            yield state, reward
+            state, reward = self.transition_reward(state).sample()
 
 
 class FiniteMarkovRewardProcess(FiniteMarkovProcess[S], MarkovRewardProcess[S]):
@@ -125,28 +119,26 @@ class FiniteMarkovRewardProcess(FiniteMarkovProcess[S], MarkovRewardProcess[S]):
     transition_reward_map: SR_TransType
     reward_vec: np.ndarray
 
-    def __init__(
-            self,
-            start_state: S,
-            state_space: Sequence[S],
-            transition_reward_map: SR_TransType
-    ):
+    def __init__(self, transition_reward_map: SR_TransType):
 
-        transition_map: Dict[S, Dict[Tuple[S, float], float]] = {}
+        transition_map: Dict[S, Dict[S, float]] = {}
 
         for state, trans in self.transition_reward_map.items():
             transition_map[state] = defaultdict(float)
             for (next_state, _), probability in trans.items():
                 transition_map[state][next_state] += probability
 
-        super().__init__(start_state, state_space, transition_map)
+        super().__init__(transition_map)
 
         self.transition_reward_map = transition_reward_map
 
         self.reward_vec = np.array(
             [sum(probability * reward for (_, reward), probability in
-                 transition_reward_map[state]) for state in state_space]
+                 transition_reward_map[state]) for state in self.state_space]
         )
+
+    def transition_reward(self, state: S) -> FiniteDistribution[Tuple[S, float]]:
+        return Categorical(self.transition_reward_map[state].items())
 
     def value_function_vec(self, gamma) -> np.ndarray:
         return np.linalg.inv(
