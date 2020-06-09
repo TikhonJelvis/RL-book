e87ee2cc76dad38bd34cd9e335cae11e275cf525
Ashwin Rao <ashwin.rao@gmail.com>
2020-06-08 17:20:46 -0700

    doing the simulations by inheriting from MarkovProcess

---
+++ rl/chapter2/stock_price_mp.py
@@ -0,0 +1,176 @@
+from dataclasses import dataclass
+from typing import Optional, Mapping, Sequence, Tuple
+from collections import Counter
+import numpy as np
+from numpy.random import binomial
+import itertools
+from operator import itemgetter
+from rl.distribution import Categorical
+from rl.markov_process import MarkovProcess
+from rl.gen_utils.common_funcs import get_logistic_func, get_unit_sigmoid_func
+from rl.chapter2.stock_price_simulations import\
+    plot_single_trace_all_processes
+from rl.chapter2.stock_price_simulations import\
+    plot_distribution_at_time_all_processes
+
+handy_map: Mapping[Optional[bool], int] = {True: -1, False: 1, None: 0}
+
+
+@dataclass
+class StateMP1:
+    price: int
+
+
+@dataclass
+class StockPriceMP1(MarkovProcess[StateMP1]):
+
+    level_param: int  # level to which price mean-reverts
+    alpha1: float = 0.25  # strength of mean-reversion (non-negative value)
+
+    def up_prob(self, state: StateMP1) -> float:
+        return get_logistic_func(self.alpha1)(self.level_param - state.price)
+
+    def transition(self, state: StateMP1) -> Categorical[StateMP1]:
+        up_p = self.up_prob(state)
+
+        return Categorical([
+            (StateMP1(state.price + 1), up_p),
+            (StateMP1(state.price - 1), 1 - up_p)
+        ])
+
+
+@dataclass
+class StateMP2:
+    price: int
+    is_prev_move_up: Optional[bool]
+
+
+@dataclass
+class StockPriceMP2(MarkovProcess[StateMP2]):
+
+    alpha2: float = 0.75  # strength of reverse-pull (value in  [0,1])
+
+    def up_prob(self, state: StateMP2) -> float:
+        return 0.5 * (1 + self.alpha2 * handy_map[state.is_prev_move_up])
+
+    def transition(self, state: StateMP2) -> Categorical[StateMP2]:
+        up_p = self.up_prob(state)
+
+        return Categorical([
+            (StateMP2(state.price + 1, True), up_p),
+            (StateMP2(state.price - 1, False), 1 - up_p)
+        ])
+
+
+@dataclass
+class StateMP3:
+    num_up_moves: int
+    num_down_moves: int
+
+
+@dataclass
+class StockPriceMP3(MarkovProcess[StateMP3]):
+
+    alpha3: float = 1.0  # strength of reverse-pull (non-negative value)
+
+    def up_prob(self, state: StateMP3) -> float:
+        total = state.num_up_moves + state.num_down_moves
+        return get_unit_sigmoid_func(self.alpha3)(
+            state.num_down_moves / total
+        ) if total else 0.5
+
+    def transition(self, state: StateMP3) -> Categorical[StateMP3]:
+        up_p = self.up_prob(state)
+
+        return Categorical([
+            (StateMP3(state.num_up_moves + 1, state.num_down_moves), up_p),
+            (StateMP3(state.num_up_moves, state.num_down_moves + 1), 1 - up_p)
+        ])
+
+
+def process1_price_traces(
+    start_price: int,
+    level_param: int,
+    alpha1: float,
+    time_steps: int,
+    num_traces: int
+) -> np.ndarray:
+    mp = StockPriceMP1(level_param=level_param, alpha1=alpha1)
+    start_state = StateMP1(price=start_price)
+    return np.vstack([
+        np.fromiter((s.price for s in itertools.islice(
+            mp.simulate(start_state),
+            time_steps + 1
+        )), float) for _ in range(num_traces)])
+
+
+def process2_price_traces(
+    start_price: int,
+    alpha2: float,
+    time_steps: int,
+    num_traces: int
+) -> np.ndarray:
+    mp = StockPriceMP2(alpha2=alpha2)
+    start_state = StateMP2(price=start_price, is_prev_move_up=None)
+    return np.vstack([
+        np.fromiter((s.price for s in itertools.islice(
+            mp.simulate(start_state),
+            time_steps + 1
+        )), float) for _ in range(num_traces)])
+
+
+def process3_price_traces(
+    start_price: int,
+    alpha3: float,
+    time_steps: int,
+    num_traces: int
+) -> np.ndarray:
+    mp = StockPriceMP3(alpha3=alpha3)
+    start_state = StateMP3(num_up_moves=0, num_down_moves=0)
+    return np.vstack([
+        np.fromiter((start_price + s.num_up_moves - s.num_down_moves
+                    for s in itertools.islice(mp.simulate(start_state),
+                                              time_steps + 1)), float)
+        for _ in range(num_traces)])
+
+
+if __name__ == '__main__':
+    start_price: int = 100
+    level_param: int = 100
+    alpha1: float = 0.25
+    alpha2: float = 0.75
+    alpha3: float = 1.0
+    time_steps: int = 100
+    num_traces: int = 1000
+
+    process1_traces: np.ndarray = process1_price_traces(
+        start_price=start_price,
+        level_param=level_param,
+        alpha1=alpha1,
+        time_steps=time_steps,
+        num_traces=num_traces
+    )
+    process2_traces: np.ndarray = process2_price_traces(
+        start_price=start_price,
+        alpha2=alpha2,
+        time_steps=time_steps,
+        num_traces=num_traces
+    )
+    process3_traces: np.ndarray = process3_price_traces(
+        start_price=start_price,
+        alpha3=alpha3,
+        time_steps=time_steps,
+        num_traces=1
+    )
+
+    trace1 = process1_traces[0]
+    trace2 = process2_traces[0]
+    trace3 = process3_traces[0]
+
+    plot_single_trace_all_processes(trace1, trace2, trace3)
+
+    plot_distribution_at_time_all_processes(
+        process1_traces,
+        process2_traces,
+        process3_traces
+    )
