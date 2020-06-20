ed4c3e61c550af5139de2b45e2631f62e9c9697b
Ashwin Rao <ashwin.rao@gmail.com>
2020-06-19 17:39:38 -0700

    first versions of Simple Inventory MDP, one with Capacity and one without Capacity

---
+++ rl/chapter3/simple_inventory_mdp_cap.py
@@ -0,0 +1,108 @@
+from typing import Tuple, Mapping, Dict, List
+from rl.markov_decision_process import FiniteMarkovDecisionProcess
+from rl.markov_decision_process import FinitePolicy, ActionMapping
+from rl.markov_process import FiniteMarkovProcess, FiniteMarkovRewardProcess
+from rl.distribution import Categorical, Constant
+from scipy.stats import poisson
+
+IntPair = Tuple[int, int]
+InvOrderMapping = Mapping[IntPair, ActionMapping[int, IntPair]]
+
+
+class SimpleInventoryMDPCap(FiniteMarkovDecisionProcess[IntPair, int]):
+
+    def __init__(
+        self,
+        capacity: int,
+        poisson_lambda: float,
+        holding_cost: float,
+        stockout_cost: float
+    ):
+        self.capacity: int = capacity
+        self.poisson_lambda: float = poisson_lambda
+        self.holding_cost: float = holding_cost
+        self.stockout_cost: float = stockout_cost
+
+        self.poisson_distr = poisson(poisson_lambda)
+        super().__init__(self.get_action_transition_reward_map())
+
+    def get_action_transition_reward_map(self) -> InvOrderMapping:
+        d: Dict[IntPair, Dict[int, Categorical[Tuple[IntPair, float]]]] = {}
+
+        for alpha in range(self.capacity + 1):
+            for beta in range(self.capacity + 1 - alpha):
+                ip = alpha + beta
+                base_reward = - self.holding_cost * alpha
+                d1: Dict[int, Categorical[Tuple[IntPair, float]]] = {}
+
+                for order in range(max(self.capacity - ip, 0) + 1):
+                    sr_probs_list: List[Tuple[Tuple[IntPair, float], float]] =\
+                        [(((ip - i, order), base_reward),
+                          self.poisson_distr.pmf(i)) for i in range(ip)]
+
+                    probability = 1 - self.poisson_distr.cdf(ip - 1)
+                    reward = base_reward - self.stockout_cost *\
+                        (probability * (self.poisson_lambda - ip) +
+                         ip * self.poisson_distr.pmf(ip))
+                    sr_probs_list.append(
+                        (((0, order), reward), probability)
+                    )
+                    d1[order] = Categorical(sr_probs_list)
+
+                d[(alpha, beta)] = d1
+        return d
+
+
+if __name__ == '__main__':
+    user_capacity = 2
+    user_poisson_lambda = 1.0
+    user_holding_cost = 1.0
+    user_stockout_cost = 10.0
+
+    user_gamma = 0.9
+
+    si_mdp: FiniteMarkovDecisionProcess[IntPair, int] = SimpleInventoryMDPCap(
+        capacity=user_capacity,
+        poisson_lambda=user_poisson_lambda,
+        holding_cost=user_holding_cost,
+        stockout_cost=user_stockout_cost
+    )
+
+    print("MDP Transition Map")
+    print("------------------")
+    print(si_mdp)
+
+    fdp: FinitePolicy[IntPair, int] = FinitePolicy(
+        {(alpha, beta): Constant(max(user_capacity - (alpha + beta), 0))
+         for alpha in range(user_capacity + 1)
+         for beta in range(user_capacity + 1 - alpha)}
+    )
+
+    print("Policy Map")
+    print("----------")
+    print(fdp)
+
+    implied_mrp: FiniteMarkovRewardProcess[IntPair] =\
+        si_mdp.apply_finite_policy(fdp)
+    print("Transition Map")
+    print("--------------")
+    print(FiniteMarkovProcess(implied_mrp.transition_map))
+
+    print("Transition Reward Map")
+    print("---------------------")
+    print(implied_mrp)
+
+    print("Stationary Distribution")
+    print("-----------------------")
+    implied_mrp.display_stationary_distribution()
+    print()
+
+    print("Reward Function")
+    print("---------------")
+    implied_mrp.display_reward_function()
+    print()
+
+    print("Value Function")
+    print("--------------")
+    implied_mrp.display_value_function(gamma=user_gamma)
+    print()
--- rl/chapter3/simple_inventory_mdp_nocap.py
+++ rl/chapter3/simple_inventory_mdp_nocap.py
@@ -4,42 +4,10 @@ from rl.markov_process import MarkovRewardProcess
 from rl.markov_decision_process import Policy
 from rl.distribution import Constant, SampledDistribution
 import numpy as np
+from scipy.stats import poisson
+import random
 
 IntPair = Tuple[int, int]
-
-
-class SimpleInventoryMRPNoCap(MarkovRewardProcess[IntPair]):
-
-    def __init__(
-        self,
-        poisson_lambda: float,
-        holding_cost: float,
-        stockout_cost: float,
-        policy: Policy[IntPair, int]
-    ):
-        self.poisson_lambda: float = poisson_lambda
-        self.holding_cost: float = holding_cost
-        self.stockout_cost: float = stockout_cost
-        self.policy: Policy[IntPair, int] = policy
-
-    def transition_reward(
-        self,
-        state: IntPair
-    ) -> SampledDistribution[Tuple[IntPair, float]]:
-        order = self.policy.act(state).sample()
-
-        def sample_next_state_reward(
-            state=state,
-            order=order
-        ) -> Tuple[IntPair, float]:
-            demand_sample = np.random.poisson(self.poisson_lambda)
-            ip = state[0] + state[1]
-            next_state = (max(ip - demand_sample, 0), order)
-            reward = - self.holding_cost * state[0]\
-                - self.stockout_cost * max(demand_sample - ip, 0)
-            return next_state, reward
-
-        return SampledDistribution(sample_next_state_reward)
 
 
 class SimpleInventoryMDPNoCap(MarkovDecisionProcess[IntPair, int]):
@@ -58,12 +26,53 @@ class SimpleInventoryMDPNoCap(MarkovDecisionProcess[IntPair, int]):
         self,
         policy: Policy[IntPair, int]
     ) -> MarkovRewardProcess[IntPair]:
-        return SimpleInventoryMRPNoCap(
-            poisson_lambda=self.poisson_lambda,
-            holding_cost=self.holding_cost,
-            stockout_cost=self.stockout_cost,
-            policy=policy
-        )
+
+        mdp = self
+
+        class ImpliedMRP(MarkovRewardProcess[IntPair]):
+
+            def transition_reward(
+                self,
+                state: IntPair
+            ) -> SampledDistribution[Tuple[IntPair, float]]:
+                order = policy.act(state).sample()
+
+                def sample_next_state_reward(
+                    mdp=mdp,
+                    state=state,
+                    order=order
+                ) -> Tuple[IntPair, float]:
+                    demand_sample = np.random.poisson(mdp.poisson_lambda)
+                    ip = state[0] + state[1]
+                    next_state = (max(ip - demand_sample, 0), order)
+                    reward = - mdp.holding_cost * state[0]\
+                        - mdp.stockout_cost * max(demand_sample - ip, 0)
+                    return next_state, reward
+
+                return SampledDistribution(sample_next_state_reward)
+
+        return ImpliedMRP()
+
+    def fraction_of_days_oos(
+        self,
+        policy: Policy[IntPair, int],
+        time_steps: int,
+        num_traces: int
+    ) -> float:
+        impl_mrp = self.apply_policy(policy)
+        count = 0
+        high_fractile = int(poisson(self.poisson_lambda).ppf(0.98))
+        start = random.choice([(i, 0) for i in range(high_fractile + 1)])
+        for _ in range(num_traces):
+            sr_pairs: Sequence[Tuple[IntPair, float]] =\
+                list(itertools.islice(
+                    impl_mrp.simulate_reward(start),
+                    time_steps + 1
+                ))
+            for i, (_, reward) in enumerate(sr_pairs[1:]):
+                if reward < - self.holding_cost * sr_pairs[i][0][0]:
+                    count += 1
+        return float(count) / (time_steps * num_traces)
 
 
 class SimpleInventoryDeterministicPolicy(Policy[IntPair, int]):
@@ -94,20 +103,15 @@ class SimpleInventoryStochasticPolicy(Policy[IntPair, int]):
 
 if __name__ == '__main__':
     import itertools
-    user_poisson_lambda = 1.0
+    user_poisson_lambda = 2.0
     user_holding_cost = 1.0
     user_stockout_cost = 10.0
 
-    user_reorder_point = 2
-    user_reorder_point_poisson_mean = 2.0
+    user_reorder_point = 8
+    user_reorder_point_poisson_mean = 8.0
 
-    user_start_state = (5, 2)
-
-    user_time_steps = 100
-
-    si_dp = SimpleInventoryDeterministicPolicy(
-        reorder_point=user_reorder_point
-    )
+    user_time_steps = 1000
+    user_num_traces = 1000
 
     si_mdp_nocap = SimpleInventoryMDPNoCap(
         poisson_lambda=user_poisson_lambda,
@@ -115,22 +119,26 @@ if __name__ == '__main__':
         stockout_cost=user_stockout_cost
     )
 
-    si_mrp_nocap_dp = si_mdp_nocap.apply_policy(si_dp)
+    si_dp = SimpleInventoryDeterministicPolicy(
+        reorder_point=user_reorder_point
+    )
 
-    trace_dp: Sequence[Tuple[IntPair, float]] = list(itertools.islice(
-        si_mrp_nocap_dp.simulate_reward(user_start_state),
-        user_time_steps + 1
-    ))
-    print(trace_dp)
+    oos_frac_dp = si_mdp_nocap.fraction_of_days_oos(
+        policy=si_dp,
+        time_steps=user_time_steps,
+        num_traces=user_num_traces
+    )
+    print("Deterministic Policy yields %.2f%% of Out-Of-Stock days" %
+          (oos_frac_dp * 100))
 
     si_sp = SimpleInventoryStochasticPolicy(
         reorder_point_poisson_mean=user_reorder_point_poisson_mean
     )
 
-    si_mrp_nocap_sp = si_mdp_nocap.apply_policy(si_sp)
-
-    trace_sp: Sequence[Tuple[IntPair, float]] = list(itertools.islice(
-        si_mrp_nocap_sp.simulate_reward(user_start_state),
-        user_time_steps + 1
-    ))
-    print(trace_sp)
+    oos_frac_sp = si_mdp_nocap.fraction_of_days_oos(
+        policy=si_sp,
+        time_steps=user_time_steps,
+        num_traces=user_num_traces
+    )
+    print("Stochastic Policy yields %.2f%% of Out-Of-Stock days" %
+          (oos_frac_sp * 100))
