d3acbe5b6d1c90cb17d03ed53d981ef08fcb4af2
Tikhon Jelvis <tikhon.jelvis@target.com>
2020-07-09 12:33:16 -0700

    Extracts iteration code into rl.iterate module, with tests

--- rl/dynamic_programming.py
+++ rl/dynamic_programming.py
@@ -1,9 +1,11 @@
 from rl.markov_decision_process import (FiniteMarkovDecisionProcess,
                                         FiniteMarkovRewardProcess)
-from typing import Callable, Mapping, Iterator, TypeVar, List, Tuple, Dict
+from typing import Mapping, TypeVar, List, Tuple, Dict
+import operator
+
+from rl.iterate import converge, iterate
 from rl.markov_decision_process import FinitePolicy
 from rl.distribution import FiniteDistribution, Categorical
-import operator
 
 A = TypeVar('A')
 S = TypeVar('S')
@@ -14,45 +16,6 @@ X = TypeVar('X')
 V = Mapping[S, float]
 
 DEFAULT_TOLERANCE = 1e-5
-
-
-# It would be more efficient if you iterated in place instead of
-# returning a copy of the value each time, but the functional version
-# of the code is a lot cleaner and easier to work with.
-def iterate(step: Callable[[X], X], start: X) -> Iterator[X]:
-    '''Find the fixed point of a function f by applying it to its own
-    result, yielding each intermediate value.
-
-    That is, for a function f, iterate(f, x) will give us a generator
-    producing:
-
-    x, f(x), f(f(x)), f(f(f(x)))...
-
-    '''
-    state = start
-
-    while True:
-        yield state
-        state = step(state)
-
-
-def converge(values: Iterator[X], done: Callable[[X, X], bool]) -> Iterator[X]:
-    '''Read from a stream of values until two consecutive values satisfy
-    the given done function.
-
-    Will error out if the stream runs out before the predicate is
-    satisfied (including streams with 0 or 1 values) and will loop
-    forever if the stream doesn't end *or* converge.
-
-    '''
-    a = next(values)
-
-    for b in values:
-        if done(a, b):
-            break
-        else:
-            a = b
-            yield b
 
 
 def condition_vf_dict(v1: V[S], v2: V[S]) -> bool:
---
+++ rl/iterate.py
@@ -0,0 +1,65 @@
+from typing import Callable, Iterator, TypeVar
+
+X = TypeVar('X')
+
+
+# It would be more efficient if you iterated in place instead of
+# returning a copy of the value each time, but the functional version
+# of the code is a lot cleaner and easier to work with.
+def iterate(step: Callable[[X], X], start: X) -> Iterator[X]:
+    '''Find the fixed point of a function f by applying it to its own
+    result, yielding each intermediate value.
+
+    That is, for a function f, iterate(f, x) will give us a generator
+    producing:
+
+    x, f(x), f(f(x)), f(f(f(x)))...
+
+    '''
+    state = start
+
+    while True:
+        yield state
+        state = step(state)
+
+
+def last(values: Iterator[X]) -> X:
+    '''Return the last value of the given iterator.
+
+    Raises an error if the iterator is empty.
+
+    If the iterator does not end, this function will loop forever.
+    '''
+    *_, last_element = values
+    return last_element
+
+
+def converge(values: Iterator[X], done: Callable[[X, X], bool]) -> Iterator[X]:
+    '''Read from an iterator until two consecutive values satisfy the
+    given done function or the input iterator ends.
+
+    Raises an error if the input iterator is empty.
+
+    Will loop forever if the input iterator doesn't end *or* converge.
+
+    '''
+    a = next(values)
+    yield a
+
+    for b in values:
+        if done(a, b):
+            break
+        else:
+            a = b
+            yield b
+
+
+def converged(values: Iterator[X], done: Callable[[X, X], bool]) -> X:
+    '''Return the final value of the given iterator when its values
+    converge according to the done function.
+
+    Raises an error if the iterator is empty.
+
+    Will loop forever if the input iterator doesn't end *or* converge.
+    '''
+    return last(converge(values, done))
