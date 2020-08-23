0260cd354b9b6382e0f63ed7bcd76760a8287adc
Ashwin Rao <ashwin.rao@gmail.com>
2020-08-22 17:34:51 -0700

    Added code for backward induction for MRP Evaluation in main

--- rl/chapter4/clearance_pricing_mdp.py
+++ rl/chapter4/clearance_pricing_mdp.py
@@ -1,10 +1,11 @@
 from rl.markov_decision_process import (
-    FiniteMarkovDecisionProcess, FinitePolicy)
+    FiniteMarkovDecisionProcess, FiniteMarkovRewardProcess, FinitePolicy)
 from rl.finite_horizon import WithTime
 from typing import Sequence, Tuple, Iterator
 from scipy.stats import poisson
-from rl.distribution import Categorical
+from rl.distribution import Categorical, Constant
 from rl.finite_horizon import (
+    finite_horizon_MRP, unwrap_finite_horizon_MRP, evaluate,
     finite_horizon_MDP, unwrap_finite_horizon_MDP, optimal_vf_and_policy)
 from rl.dynamic_programming import V
 
@@ -14,6 +15,7 @@ class ClearancePricingMDP:
     initial_inventory: int
     time_steps: int
     price_lambda_pairs: Sequence[Tuple[float, float]]
+    single_step_mdp: FiniteMarkovDecisionProcess[int, int]
     mdp: FiniteMarkovDecisionProcess[WithTime[int], int]
 
     def __init__(
@@ -27,7 +29,7 @@ class ClearancePricingMDP:
         self.price_lambda_pairs = price_lambda_pairs
         distrs = [poisson(l) for _, l in price_lambda_pairs]
         prices = [p for p, _ in price_lambda_pairs]
-        stationary_mdp: FiniteMarkovDecisionProcess[int, int] =\
+        self.single_step_mdp: FiniteMarkovDecisionProcess[int, int] =\
             FiniteMarkovDecisionProcess({
                 s: {i: Categorical(
                     {(s - k, prices[i] * k):
@@ -36,7 +38,15 @@ class ClearancePricingMDP:
                     for i in range(len(prices))}
                 for s in range(initial_inventory + 1)
             })
-        self.mdp = finite_horizon_MDP(stationary_mdp, time_steps)
+        self.mdp = finite_horizon_MDP(self.single_step_mdp, time_steps)
+
+    def get_vf_for_policy(
+        self,
+        policy: FinitePolicy[WithTime[int], int]
+    ) -> Iterator[V[int]]:
+        mrp: FiniteMarkovRewardProcess[WithTime[int]] \
+            = self.mdp.apply_finite_policy(policy)
+        return evaluate(unwrap_finite_horizon_MRP(mrp))
 
     def get_optimal_vf_and_policy(self)\
             -> Iterator[Tuple[V[int], FinitePolicy[int, int]]]:
@@ -52,7 +62,33 @@ if __name__ == '__main__':
         time_steps=steps,
         price_lambda_pairs=pairs
     )
+    print("Clearance Pricing MDP")
+    print("---------------------")
     print(cp.mdp)
+
+    def policy_func(x: int) -> int:
+        return 0 if x < 2 else (1 if x < 5 else (2 if x < 8 else 3))
+
+    stationary_policy: FinitePolicy[int, int] = FinitePolicy(
+        {s: Constant(policy_func(s)) for s in range(ii + 1)}
+    )
+
+    single_step_mrp: FiniteMarkovRewardProcess[int] = \
+        cp.single_step_mdp.apply_finite_policy(stationary_policy)
+
+    vf_for_policy: Iterator[V[int]] = evaluate(unwrap_finite_horizon_MRP(
+        finite_horizon_MRP(single_step_mrp, steps)
+    ))
+
+    print("Value Function for Stationary Policy")
+    print("------------------------------------")
+    for t, vf in enumerate(vf_for_policy):
+        print(f"Time Step {t:d}")
+        print("---------------")
+        print(vf)
+
+    print("Optimal Value Function and Optimal Policy")
+    print("------------------------------------")
     for t, (vf, policy) in enumerate(cp.get_optimal_vf_and_policy()):
         print(f"Time Step {t:d}")
         print("---------------")
