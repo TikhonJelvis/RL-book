acd20f05804f21599a5d2f90285a8816f5aaa225
Ashwin Rao <ashwin.rao@gmail.com>
2020-11-22 09:27:54 -0800

    changed notation to make it consistent with the chapter notation

--- rl/chapter8/optimal_exercise_bin_tree.py
+++ rl/chapter8/optimal_exercise_bin_tree.py
@@ -35,8 +35,8 @@ class OptimalExerciseBinTree:
     def dt(self) -> float:
         return self.expiry / self.num_steps
 
-    def state_price(self, t: int, i: int) -> float:
-        return self.spot_price * np.exp((2 * i - t) * self.vol *
+    def state_price(self, i: int, j: int) -> float:
+        return self.spot_price * np.exp((2 * j - i) * self.vol *
                                         np.sqrt(self.dt()))
 
     def get_opt_vf_and_policy(self) -> \
@@ -47,23 +47,23 @@ class OptimalExerciseBinTree:
             (up_factor * up_factor - 1)
         return optimal_vf_and_policy(
             steps=[
-                {i: None if i == -1 else {
+                {j: None if j == -1 else {
                     True: Constant(
                         (
                             -1,
-                            self.payoff(t * dt, self.state_price(t, i))
+                            self.payoff(i * dt, self.state_price(i, j))
                         )
                     ),
                     False: Categorical(
                         {
-                            (i + 1, 0.): up_prob,
-                            (i, 0.): 1 - up_prob
+                            (j + 1, 0.): up_prob,
+                            (j, 0.): 1 - up_prob
                         }
                     )
-                } for i in range(t + 1)}
-                for t in range(self.num_steps + 1)
+                } for j in range(i + 1)}
+                for i in range(self.num_steps + 1)
             ],
-            gamma=np.exp(-self.rate * self.dt())
+            gamma=np.exp(-self.rate * dt)
         )
 
     def option_exercise_boundary(
@@ -73,27 +73,27 @@ class OptimalExerciseBinTree:
     ) -> Sequence[Tuple[float, float]]:
         dt: float = self.dt()
         ex_boundary: List[Tuple[float, float]] = []
-        for t in range(self.num_steps + 1):
-            ex_points = [i for i in range(t + 1)
-                         if policy_seq[t].act(i).value and
-                         self.payoff(t * dt, self.state_price(t, i)) > 0]
+        for i in range(self.num_steps + 1):
+            ex_points = [j for j in range(i + 1)
+                         if policy_seq[i].act(j).value and
+                         self.payoff(i * dt, self.state_price(i, j)) > 0]
             if len(ex_points) > 0:
                 boundary_pt = min(ex_points) if is_call else max(ex_points)
                 ex_boundary.append(
-                    (t * dt, opt_ex_bin_tree.state_price(t, boundary_pt))
+                    (i * dt, opt_ex_bin_tree.state_price(i, boundary_pt))
                 )
         return ex_boundary
 
 
 if __name__ == '__main__':
-    import matplotlib.pyplot as plt
+    from rl.gen_utils.plot_funcs import plot_list_of_curves
     spot_price_val: float = 100.0
     strike: float = 100.0
-    is_call: bool = False
-    expiry_val: float = 2.0
+    is_call: bool = True
+    expiry_val: float = 1.0
     rate_val: float = 0.05
     vol_val: float = 0.25
-    num_steps_val: int = 500
+    num_steps_val: int = 300
 
     if is_call:
         opt_payoff = lambda _, x: max(x - strike, 0)
@@ -113,9 +113,16 @@ if __name__ == '__main__':
     ex_boundary: Sequence[Tuple[float, float]] = \
         opt_ex_bin_tree.option_exercise_boundary(policy_seq, is_call)
     time_pts, ex_bound_pts = zip(*ex_boundary)
-    plt.plot(time_pts, ex_bound_pts)
-    plt.title("Optimal Exercise Boundary")
-    plt.show()
+    label = ("Call" if is_call else "Put") + " Option Exercise Boundary"
+    plot_list_of_curves(
+        list_of_x_vals=[time_pts],
+        list_of_y_vals=[ex_bound_pts],
+        list_of_colors=["b"],
+        list_of_curve_labels=[label],
+        x_label="Time",
+        y_label="Underlying Price",
+        title=label
+    )
 
     european: float = opt_ex_bin_tree.european_price(is_call, strike)
     print(f"European Price = {european:.3f}")
