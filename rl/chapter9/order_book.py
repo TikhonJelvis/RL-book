9e19922ba9f32cbc6d3d70b69c05ba9a66903d8d
Ashwin Rao <ashwin.rao@gmail.com>
2020-11-29 12:26:34 -0800

    better design

--- rl/chapter9/order_book.py
+++ rl/chapter9/order_book.py
@@ -3,7 +3,15 @@ from __future__ import annotations
 from dataclasses import dataclass, replace
 from typing import Sequence, Tuple, Optional, List
 
-PriceSizePairs = Sequence[Tuple[float, int]]
+
+@dataclass(frozen=True)
+class DollarsAndShares:
+
+    dollars: float
+    shares: int
+
+
+PriceSizePairs = Sequence[DollarsAndShares]
 
 
 @dataclass(frozen=True)
@@ -13,10 +21,10 @@ class OrderBook:
     ascending_asks: PriceSizePairs
 
     def bid_price(self) -> float:
-        return self.descending_bids[0][0]
+        return self.descending_bids[0].dollars
 
     def ask_price(self) -> float:
-        return self.ascending_asks[0][0]
+        return self.ascending_asks[0].dollars
 
     def mid_price(self) -> float:
         return (self.bid_price() + self.ask_price()) / 2
@@ -25,49 +33,80 @@ class OrderBook:
         return self.ask_price() - self.bid_price()
 
     def market_depth(self) -> float:
-        return self.ascending_asks[-1][0] - self.descending_bids[-1][0]
+        return self.ascending_asks[-1].dollars - \
+            self.descending_bids[-1].dollars
 
     @staticmethod
     def eat_book(
-        book: PriceSizePairs,
-        size: int
-    ) -> Tuple[int, PriceSizePairs]:
-        rem_size: int = size
-        for i, (this_price, this_size) in enumerate(book):
-            if rem_size < this_size:
-                return (0, [(this_price, this_size - rem_size)] +
-                        list(book[i+1:]))
+        ps_pairs: PriceSizePairs,
+        shares: int
+    ) -> Tuple[DollarsAndShares, PriceSizePairs]:
+        '''
+        Returned DollarsAndShares represents the pair of
+        dollars transacted and the number of shares transacted
+        on ps_pairs (with number of shares transacted being less
+        than or equal to the input shares).
+        Returned PriceSizePairs represents the remainder of the
+        ps_pairs after the transacted number of shares have eaten into
+        the input ps_pairs.
+        '''
+        rem_shares: int = shares
+        dollars: float = 0.
+        for i, d_s in enumerate(ps_pairs):
+            this_price: float = d_s.dollars
+            this_shares: int = d_s.shares
+            dollars += this_price * min(rem_shares, this_shares)
+            if rem_shares < this_shares:
+                return (
+                    DollarsAndShares(dollars=dollars, shares=shares),
+                    [DollarsAndShares(
+                        dollars=this_price,
+                        shares=this_shares - rem_shares
+                    )] + list(ps_pairs[i+1:])
+                )
             else:
-                rem_size -= this_size
+                rem_shares -= this_shares
 
-        return (rem_size, [])
+        return (
+            DollarsAndShares(dollars=dollars, shares=shares - rem_shares),
+            []
+        )
 
-    def sell_limit_order(self, price: float, size: int) -> OrderBook:
-        index: Optional[int] = next((i for i, (p, _)
+    def sell_limit_order(self, price: float, shares: int) -> OrderBook:
+        index: Optional[int] = next((i for i, d_s
                                      in enumerate(self.descending_bids)
-                                     if p < price), None)
+                                     if d_s.dollars < price), None)
         eligible_bids: PriceSizePairs = self.descending_bids \
             if index is None else self.descending_bids[:index]
         ineligible_bids: PriceSizePairs = [] if index is None else \
             self.descending_bids[index:]
 
-        rem_size, rem_bids = OrderBook.eat_book(eligible_bids, size)
-
+        d_s, rem_bids = OrderBook.eat_book(eligible_bids, shares)
         new_bids: PriceSizePairs = list(rem_bids) + list(ineligible_bids)
-        if rem_size > 0:
-            asks_copy: List[Tuple[float, int]] = list(self.ascending_asks)
-            index1: Optional[int] = next((i for i, (p, _)
-                                          in enumerate(asks_copy)
-                                          if p >= price), None)
+        rem_shares: int = shares - d_s.shares
+
+        if rem_shares > 0:
+            new_asks: List[DollarsAndShares] = list(self.ascending_asks)
+            index1: Optional[int] = next((i for i, d_s
+                                          in enumerate(new_asks)
+                                          if d_s.dollars >= price), None)
             if index1 is None:
-                asks_copy.append((price, rem_size))
-            elif asks_copy[index1][0] != price:
-                asks_copy.insert(index1, (price, rem_size))
+                new_asks.append(DollarsAndShares(
+                    dollars=price,
+                    shares=rem_shares
+                ))
+            elif new_asks[index1].dollars != price:
+                new_asks.insert(index1, DollarsAndShares(
+                    dollars=price,
+                    shares=rem_shares
+                ))
             else:
-                asks_copy[index1] = (price, asks_copy[index1][1] + rem_size)
-            return replace(
-                self,
-                ascending_asks=asks_copy,
+                new_asks[index1] = DollarsAndShares(
+                    dollars=price,
+                    shares=new_asks[index1].shares + rem_shares
+                )
+            return OrderBook(
+                ascending_asks=new_asks,
                 descending_bids=new_bids
             )
         else:
@@ -76,40 +115,53 @@ class OrderBook:
                 descending_bids=new_bids
             )
 
-    def sell_market_order(self, size: int) -> Tuple[int, OrderBook]:
-        rem_size, rem_bids = OrderBook.eat_book(
+    def sell_market_order(
+        self,
+        shares: int
+    ) -> Tuple[DollarsAndShares, OrderBook]:
+        d_s, rem_bids = OrderBook.eat_book(
             self.descending_bids,
-            size
+            shares
         )
-        return (rem_size, replace(self, descending_bids=rem_bids))
+        return (d_s, replace(self, descending_bids=rem_bids))
 
-    def buy_limit_order(self, price: float, size: int) -> OrderBook:
-        index: Optional[int] = next((i for i, (p, _)
+    def buy_limit_order(self, price: float, shares: int) -> OrderBook:
+        index: Optional[int] = next((i for i, d_s
                                      in enumerate(self.ascending_asks)
-                                     if p > price), None)
+                                     if d_s.dollars > price), None)
         eligible_asks: PriceSizePairs = self.ascending_asks \
             if index is None else self.ascending_asks[:index]
         ineligible_asks: PriceSizePairs = [] if index is None else \
             self.ascending_asks[index:]
 
-        rem_size, rem_asks = OrderBook.eat_book(eligible_asks, size)
+        d_s, rem_asks = OrderBook.eat_book(eligible_asks, shares)
         new_asks: PriceSizePairs = list(rem_asks) + list(ineligible_asks)
+        rem_shares: int = shares - d_s.shares
 
-        if rem_size > 0:
-            bids_copy: List[Tuple[float, int]] = list(self.descending_bids)
-            index1: Optional[int] = next((i for i, (p, _)
-                                          in enumerate(bids_copy)
-                                          if p <= price), None)
+        if rem_shares > 0:
+            new_bids: List[DollarsAndShares] = list(self.descending_bids)
+            index1: Optional[int] = next((i for i, d_s
+                                          in enumerate(new_bids)
+                                          if d_s.dollars <= price), None)
             if index1 is None:
-                bids_copy.append((price, rem_size))
-            elif bids_copy[index1][0] != price:
-                bids_copy.insert(index1, (price, rem_size))
+                new_bids.append(DollarsAndShares(
+                    dollars=price,
+                    shares=rem_shares
+                ))
+            elif new_bids[index1].dollars != price:
+                new_bids.insert(index1, DollarsAndShares(
+                    dollars=price,
+                    shares=rem_shares
+                ))
             else:
-                bids_copy[index1] = (price, bids_copy[index1][1] + rem_size)
+                new_bids[index1] = DollarsAndShares(
+                    dollars=price,
+                    shares=new_bids[index1].shares + rem_shares
+                )
             return replace(
                 self,
                 ascending_asks=new_asks,
-                descending_bids=bids_copy
+                descending_bids=new_bids
             )
         else:
             return replace(
@@ -117,12 +169,15 @@ class OrderBook:
                 ascending_asks=new_asks
             )
 
-    def buy_market_order(self, size: int) -> Tuple[int, OrderBook]:
-        rem_size, rem_asks = OrderBook.eat_book(
+    def buy_market_order(
+        self,
+        shares: int
+    ) -> Tuple[DollarsAndShares, OrderBook]:
+        d_s, rem_asks = OrderBook.eat_book(
             self.ascending_asks,
-            size
+            shares
         )
-        return (rem_size, replace(self, ascending_asks=rem_asks))
+        return (d_s, replace(self, ascending_asks=rem_asks))
 
     def pretty_print_order_book(self) -> None:
         from pprint import pprint
@@ -137,15 +192,18 @@ class OrderBook:
 
     def display_order_book(self) -> None:
         import matplotlib.pyplot as plt
+
+        bid_prices = [d_s.dollars for d_s in self.descending_bids]
+        bid_shares = [d_s.shares for d_s in self.descending_bids]
         if self.descending_bids:
-            bid_prices, bid_sizes = zip(*self.descending_bids)
-            plt.bar(bid_prices, bid_sizes, color='blue')
+            plt.bar(bid_prices, bid_shares, color='blue')
+
+        ask_prices = [d_s.dollars for d_s in self.ascending_asks]
+        ask_shares = [d_s.shares for d_s in self.ascending_asks]
         if self.ascending_asks:
-            ask_prices, ask_sizes = zip(*self.ascending_asks)
-            plt.bar(ask_prices, ask_sizes, color='red')
-        bid_vals = [p for p, _ in self.descending_bids]
-        ask_vals = [p for p, _ in self.ascending_asks]
-        all_prices = sorted(bid_vals + ask_vals)
+            plt.bar(ask_prices, ask_shares, color='red')
+
+        all_prices = sorted(bid_prices + ask_prices)
         all_ticks = ["%d" % x for x in all_prices]
         plt.xticks(all_prices, all_ticks)
         plt.grid(axis='y')
@@ -160,39 +218,51 @@ if __name__ == '__main__':
 
     from numpy.random import poisson
 
-    bids: PriceSizePairs = [(x, poisson(100. - (100 - x) * 10))
-                            for x in range(100, 90, -1)]
-    asks: PriceSizePairs = [(x, poisson(100. - (x - 105) * 10))
-                            for x in range(105, 115, 1)]
+    bids: PriceSizePairs = [DollarsAndShares(
+        dollars=x,
+        shares=poisson(100. - (100 - x) * 10)
+    ) for x in range(100, 90, -1)]
+    asks: PriceSizePairs = [DollarsAndShares(
+        dollars=x,
+        shares=poisson(100. - (x - 105) * 10)
+    ) for x in range(105, 115, 1)]
 
     ob0: OrderBook = OrderBook(descending_bids=bids, ascending_asks=asks)
     ob0.pretty_print_order_book()
     ob0.display_order_book()
 
-    ob1: OrderBook = ob0.sell_limit_order(107, 40)
     print("Sell Limit Order of (107, 40)")
     print()
+    ob1: OrderBook = ob0.sell_limit_order(107, 40)
     ob1.pretty_print_order_book()
     ob1.display_order_book()
 
-    _, ob2 = ob1.sell_market_order(120)
     print("Sell Market Order of 120")
     print()
+    d_s, ob2 = ob1.sell_market_order(120)
+    proceeds: float = d_s.dollars
+    shares_sold: int = d_s.shares
+    print(f"Sales Proceeds = {proceeds:.2f}, Shares Sold = {shares_sold:d}")
     ob2.pretty_print_order_book()
     ob2.display_order_book()
 
-    ob3: OrderBook = ob2.buy_limit_order(100, 80)
     print("Buy Limit Order of (100, 80)")
     print()
+    ob3: OrderBook = ob2.buy_limit_order(100, 80)
     ob3.pretty_print_order_book()
     ob3.display_order_book()
-    ob4: OrderBook = ob3.sell_limit_order(104, 60)
+
     print("Sell Limit Order of (104, 60)")
     print()
+    ob4: OrderBook = ob3.sell_limit_order(104, 60)
     ob4.pretty_print_order_book()
     ob4.display_order_book()
-    _, ob5 = ob4.buy_market_order(150)
+
     print("Buy Market Order of 150")
     print()
+    d_s, ob5 = ob4.buy_market_order(150)
+    bill: float = d_s.dollars
+    shares_bought: int = d_s.shares
+    print(f"Purchase Bill = {bill:.2f}, Shares Bought = {shares_bought:d}")
     ob5.pretty_print_order_book()
     ob5.display_order_book()
