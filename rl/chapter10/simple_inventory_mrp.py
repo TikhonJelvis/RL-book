1b9d635d3493d74a775d09b474c203bc60ab61f4
Ashwin Rao <ashwin.rao@gmail.com>
2020-12-20 16:35:53 -0800

    this code is working now

--- rl/chapter10/simple_inventory_mrp.py
+++ rl/chapter10/simple_inventory_mrp.py
@@ -1,105 +1,186 @@
-from typing import Iterator, Iterable
-from itertools import islice, chain
+from typing import Iterator, Sequence
+from itertools import islice
 from rl.chapter2.simple_inventory_mrp import SimpleInventoryMRPFinite
 from rl.chapter2.simple_inventory_mrp import InventoryState
-from rl.function_approx import Tabular, FunctionApprox
-from rl.distribution import Choose
-from rl.markov_process import TransitionStep
-import rl.monte_carlo as mc
-import rl.td as td
+from rl.function_approx import FunctionApprox
+from rl.chapter10.prediction_utils import (
+    mc_finite_prediction_equal_wts, mc_finite_prediction_learning_rate,
+    td_finite_prediction_learning_rate
+)
+from rl.iterate import last
 from rl.gen_utils.plot_funcs import plot_list_of_curves
 from math import sqrt, log, ceil
+import numpy as np
+from pprint import pprint
 
 
-user_capacity = 2
-user_poisson_lambda = 1.0
-user_holding_cost = 1.0
-user_stockout_cost = 10.0
-
-user_gamma = 0.9
-
-num_traces = 100000
-pool = 100
-
-tol = 1e-6
-
-learning_rate = 0.1
-learning_rate_decay = 1000
-
-si_mrp = SimpleInventoryMRPFinite(
-    capacity=user_capacity,
-    poisson_lambda=user_poisson_lambda,
-    holding_cost=user_holding_cost,
-    stockout_cost=user_stockout_cost
-)
-
-states = si_mrp.non_terminal_states
-true_vf = si_mrp.get_value_function_vec(gamma=user_gamma)
-
-mc_traces: Iterable[Iterable[TransitionStep[InventoryState]]] = \
-                 si_mrp.reward_traces(Choose(set(states)))
-
-
-def count_to_weight(n: int) -> float:
-    return learning_rate * (n / learning_rate_decay) ** -0.5
-
-
-mc_funcs_it: Iterator[FunctionApprox[InventoryState]] = mc.evaluate_mrp(
-    mc_traces,
-    approx_0=Tabular(count_to_weight_func=count_to_weight),
-    γ=user_gamma,
-    tolerance=tol
-)
-
-mc_errors = []
-pool_mc_errs = []
-for i, mc_f in enumerate(islice(mc_funcs_it, 0, num_traces)):
-    pool_mc_errs.append(sqrt(sum((mc_f(s) - true_vf[j]) ** 2 for j, s in
-                        enumerate(states)) / len(states)))
-    if i % pool == pool - 1:
-        mc_errors.append(sum(pool_mc_errs) / pool)
-        pool_mc_errs = []
-
-td_episode_length = int(ceil(log(tol) / log(user_gamma)))
-
-
-td_traces: Iterable[Iterable[TransitionStep[InventoryState]]] = \
-                 si_mrp.reward_traces(Choose(set(states)))
-
-td_experiences: Iterable[TransitionStep[InventoryState]] = \
-    chain.from_iterable(
-        islice(trace, td_episode_length) for trace in td_traces
+def inventory_mrp(
+    capacity: int,
+    poisson_lambda: float,
+    holding_cost: float,
+    stockout_cost: float
+) -> SimpleInventoryMRPFinite:
+    return SimpleInventoryMRPFinite(
+        capacity=capacity,
+        poisson_lambda=poisson_lambda,
+        holding_cost=holding_cost,
+        stockout_cost=stockout_cost
     )
 
-td_funcs_it: Iterator[FunctionApprox[InventoryState]] = td.evaluate_mrp(
-    td_experiences,
-    approx_0=Tabular(count_to_weight_func=count_to_weight),
-    γ=user_gamma
-)
 
-td_errors = []
-trans_pool = pool * td_episode_length
-pool_td_errs = []
+def mc_equal_wts_correctness(
+    inv_mrp: SimpleInventoryMRPFinite,
+    gamma: float,
+    tolerance: float,
+    num_episodes=int
+) -> None:
+    mc_vfs: Iterator[FunctionApprox[InventoryState]] = \
+        mc_finite_prediction_equal_wts(
+            fmrp=inv_mrp,
+            gamma=gamma,
+            tolerance=tolerance
+        )
+    final_mc_vf: FunctionApprox[InventoryState] = \
+        last(islice(mc_vfs, num_episodes))
+    print(f"Equal Weights MC Value Function with {num_episodes:d} episodes")
+    pprint({s: round(final_mc_vf(s), 3) for s in inv_mrp.non_terminal_states})
+    print("True Value Function")
+    inv_mrp.display_value_function(gamma=gamma)
 
-for i, td_f in enumerate(
-        islice(td_funcs_it, 0, num_traces * td_episode_length)
-):
-    pool_td_errs.append(sqrt(sum((td_f(s) - true_vf[j]) ** 2 for j, s in
-                        enumerate(states)) / len(states)))
-    if i % trans_pool == trans_pool - 1:
-        td_errors.append(sum(pool_td_errs) / trans_pool)
-        pool_td_errs = []
 
-plot_start = 0
-mc_plot = mc_errors[plot_start:]
-td_plot = td_errors[plot_start:]
+def td_learning_rate_correctness(
+    inv_mrp: SimpleInventoryMRPFinite,
+    gamma: float,
+    episode_length: int,
+    num_episodes: int,
+    base_learning_rate: float,
+    learning_rate_decay: float
+) -> None:
+    td_vfs: Iterator[FunctionApprox[InventoryState]] = \
+        td_finite_prediction_learning_rate(
+            fmrp=inv_mrp,
+            gamma=gamma,
+            episode_length=episode_length,
+            base_learning_rate=base_learning_rate,
+            learning_rate_decay=learning_rate_decay
+        )
+    final_td_vf: FunctionApprox[InventoryState] = \
+        last(islice(td_vfs, num_episodes))
+    print("Constant Learning Rate TD Value Function with " +
+          f"{num_episodes:d} episodes")
+    pprint({s: round(final_td_vf(s), 3) for s in inv_mrp.non_terminal_states})
+    print("True Value Function")
+    inv_mrp.display_value_function(gamma=gamma)
 
-plot_list_of_curves(
-    [range(len(mc_plot)), range(len(td_plot))],
-    [mc_plot, td_plot],
-    ["b", "r"],
-    ["Monte-Carlo", "Temporal-Difference"],
-    x_label="Episode Batches",
-    y_label="Value Function RMSE",
-    title="RMSE of MC and TD as function of episode batches"
-)
+
+def compare_td_and_mc(
+    inv_mrp: SimpleInventoryMRPFinite,
+    gamma: float,
+    mc_episode_length_tol: float,
+    num_episodes: int,
+    base_learning_rate: float,
+    learning_rate_decay: float,
+    plot_batch: int
+) -> None:
+    true_vf: np.ndarray = inv_mrp.get_value_function_vec(gamma)
+    states: Sequence[InventoryState] = inv_mrp.non_terminal_states
+    mc_funcs_it: Iterator[FunctionApprox[InventoryState]] = \
+        mc_finite_prediction_learning_rate(
+            fmrp=inv_mrp,
+            gamma=gamma,
+            tolerance=mc_episode_length_tol,
+            base_learning_rate=base_learning_rate,
+            learning_rate_decay=learning_rate_decay
+        )
+    mc_errors = []
+    batch_mc_errs = []
+    for i, mc_f in enumerate(islice(mc_funcs_it, num_episodes)):
+        batch_mc_errs.append(sqrt(sum((mc_f(s) - true_vf[j]) ** 2 for j, s in
+                                      enumerate(states)) / len(states)))
+        if i % plot_batch == plot_batch - 1:
+            mc_errors.append(sum(batch_mc_errs) / plot_batch)
+            batch_mc_errs = []
+
+    td_episode_length = int(ceil(log(mc_episode_length_tol) / log(gamma)))
+    td_funcs_it: Iterator[FunctionApprox[InventoryState]] = \
+        td_finite_prediction_learning_rate(
+            fmrp=inv_mrp,
+            gamma=gamma,
+            episode_length=td_episode_length,
+            base_learning_rate=base_learning_rate,
+            learning_rate_decay=learning_rate_decay
+        )
+    td_errors = []
+    transitions_batch = plot_batch * td_episode_length
+    batch_td_errs = []
+
+    for i, td_f in enumerate(
+            islice(td_funcs_it, num_episodes * td_episode_length)
+    ):
+        batch_td_errs.append(sqrt(sum((td_f(s) - true_vf[j]) ** 2 for j, s in
+                                      enumerate(states)) / len(states)))
+        if i % transitions_batch == transitions_batch - 1:
+            td_errors.append(sum(batch_td_errs) / transitions_batch)
+            batch_td_errs = []
+
+    plot_start = 0
+    mc_plot = mc_errors[plot_start:]
+    td_plot = td_errors[plot_start:]
+
+    plot_list_of_curves(
+        [range(len(mc_plot)), range(len(td_plot))],
+        [mc_plot, td_plot],
+        ["b", "r"],
+        ["Monte-Carlo", "Temporal-Difference"],
+        x_label="Episode Batches",
+        y_label="Value Function RMSE",
+        title="RMSE of MC and TD as function of episode batches"
+    )
+
+
+if __name__ == '__main__':
+    capacity: int = 2
+    poisson_lambda: float = 1.0
+    holding_cost: float = 1.0
+    stockout_cost: float = 10.0
+
+    gamma: float = 0.9
+    mc_episode_length_tol: float = 1e-6
+    num_episodes = 10000
+
+    base_learning_rate: float = 0.1
+    learning_rate_decay: float = 100
+
+    si_mrp: SimpleInventoryMRPFinite = inventory_mrp(
+        capacity=capacity,
+        poisson_lambda=poisson_lambda,
+        holding_cost=holding_cost,
+        stockout_cost=stockout_cost
+    )
+
+#     td_episode_length: int = 100
+#     mc_equal_wts_correctness(
+#         inv_mrp=si_mrp,
+#         gamma=gamma,
+#         tolerance=mc_episode_length_tol,
+#         num_episodes=num_episodes
+#     )
+#     td_learning_rate_correctness(
+#         inv_mrp=si_mrp,
+#         gamma=gamma,
+#         episode_length=td_episode_length,
+#         num_episodes=num_episodes,
+#         base_learning_rate=base_learning_rate,
+#         learning_rate_decay=learning_rate_decay
+#     )
+
+    plot_batch: int = 100
+    compare_td_and_mc(
+        inv_mrp=si_mrp,
+        gamma=gamma,
+        mc_episode_length_tol=mc_episode_length_tol,
+        num_episodes=num_episodes,
+        base_learning_rate=base_learning_rate,
+        learning_rate_decay=learning_rate_decay,
+        plot_batch=plot_batch
+    )
