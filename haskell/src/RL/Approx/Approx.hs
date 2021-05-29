3a29811c8b5255e8662a2ceb8d5975e2a09014d6
Tikhon Jelvis <tikhon@jelv.is>
2021-05-28 17:28:10 -0700

    Revised Approx interface based on conversation with Paritosh

--- haskell/src/RL/Approx/Approx.hs
+++ haskell/src/RL/Approx/Approx.hs
@@ -20,6 +20,15 @@ import           RL.Vector                                ( Affine(..)
                                                           , VectorSpace(..)
                                                           )
 
+-- | A batch of x, y observations.
+data Batch a = Batch
+  { xs :: !(V.Vector a)
+    -- ^ X values, same length as ys
+  , ys :: !(Vector R)
+    -- ^ Corresponding Y values, same length as xs
+  }
+
+
 -- | Updatable approximations for functions of the type @a → ℝ@.
 class Affine (f a) => Approx f a where
   -- | Evaluate the function approximation as a function.
@@ -28,25 +37,22 @@ class Affine (f a) => Approx f a where
   -- @a → ℝ@.
   eval :: f a -> (a -> R)
 
-  -- | Find an approximation that's reasonably close to the given set
-  -- of x, y pairs.
-  fit :: f a
-      -- ^ Starting configuration, if needed. Should probably ignore
-      -- any "updatable" elements like weights... etc.
-      -> V.Vector a
-      -- ^ x values—should map 1:1 to y values
-      -> Vector R
-      -- ^ y values—should map 1:1 to x values
-      -> f a
-
   -- | Evaluate a whole bunch of inputs and produce a vector of the
   -- results.
   eval' :: f a -> (V.Vector a -> Vector R)
   eval' f xs = Matrix.storable (eval f <$> xs)
 
-  -- | Improve the function approximation given a list of
-  -- observations. The two vectors have to be the same length and
-  -- match up one-to-one.
+  -- | Given an existing approximation and a batch of additional
+  -- points produces a @Diff (f a)@ that would move the existing
+  -- observation to better represent the new observations.
+  direction :: f a
+            -- ^ Existing function approximation.
+            -> Batch a
+            -- ^ A batch of new observations.
+            -> Diff (f a)
+
+  -- | Improve the function approximation given a batch of additional
+  -- x, y points.
   update :: Scalar (Diff (f a))
          -- ^ α: Learning rate or how much to weigh new
          -- information. @α = 0@ means an update does nothing; @α = 1@
@@ -54,9 +60,7 @@ class Affine (f a) => Approx f a where
          -- observations.
          -> f a
          -- ^ Function approximation to start from.
-         -> V.Vector a
-         -- ^ x values to update
-         -> Vector R
-         -- ^ y values for each x value to update
+         -> Batch a
+         -- ^ Batch of new points to update the approximation.
          -> f a
-  update α f x y = f .+ (α *: (fit f x y .-. f))
+  update α f new = f .+ (α *: direction f new)
