c01c6546c6e4ea01eaa8007a894398c672a2e56e
Tikhon Jelvis <tikhon@jelv.is>
2021-11-13 18:04:50 -0800

    Extended Alon.cls quote filter with a fallback for non-LaTeX formats

--- bin/alon-style-quotes.lua
+++ bin/alon-style-quotes.lua
@@ -1,14 +1,27 @@
 -- Replace block quotes with quotes using a custom class from Alon.cls
-if FORMAT:match 'latex' then
-   function CodeBlock(block)
-      if block.attributes.quoteBy and block.attributes.quoteFrom then
-         local by = block.attributes.quoteBy
-         local from = block.attributes.quoteFrom
+function CodeBlock(block)
+   if block.attributes.quoteBy and block.attributes.quoteFrom then
+      local by = block.attributes.quoteBy
+      local from = block.attributes.quoteFrom
+
+      if FORMAT:match 'latex' then
          return {
             pandoc.RawBlock('latex', '\\begin{VT1}'),
             pandoc.Para(block.text),
             pandoc.RawBlock('latex', '\\VTA{' .. by .. '}{' .. from .. '}'),
             pandoc.RawBlock('latex', '\\end{VT1}'),
+         }
+      else
+         return {
+            pandoc.BlockQuote(block.text),
+            pandoc.BulletList({
+                  {
+                     pandoc.Para({
+                           pandoc.Str(by .. ", "),
+                           pandoc.Emph(from)
+                     })
+                  }
+            })
          }
       end
    end
