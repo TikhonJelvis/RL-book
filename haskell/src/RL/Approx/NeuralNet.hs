d89d545b3e0024b242a8c4bed6210ec5b93e99bf
Paritosh Desai <paritosh.desai@gmail.com>
2022-01-21 17:56:56 -0800

    Adding support for actual scoringfunction in NeuralNetwork and a trivial test

--- haskell/src/RL/Approx/NeuralNet.hs
+++ haskell/src/RL/Approx/NeuralNet.hs
@@ -3,6 +3,8 @@
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE OverloadedLists #-}
+
 -- | Use Neural Network to approximate the value function
 
 
@@ -11,6 +13,7 @@ module RL.Approx.NeuralNet where
 
 import           Numeric.LinearAlgebra                    ( (#>)
                                                           , (<.>)
+                                                          , (><)
                                                           , Matrix
                                                           , R
                                                           , Vector
@@ -37,9 +40,38 @@ data Layer = Layer
     activation :: (R -> R) 
   }
 
-scoreLayer :: Vector R -> Layer -> Vector R
-scoreLayer input Layer { weights, activation } = cmap activation ( tr' weights #> input )
+scoreLayer :: Layer -> Vector R -> Vector R
+scoreLayer Layer { weights, activation } input = cmap activation ( tr' weights #> input )
 
 -- Need to look at the foldr and needing to flip the order
 scoreNeuralNetwork :: Vector R -> [Layer] -> Vector R
+-- scoreNeuralNetwork input lls = foldr (.) id (map (flip scoreLayer lls) input 
 scoreNeuralNetwork input lls = foldr (.) id (map scoreLayer lls) input 
+
+
+-- Create a simple test
+-- input x = [1, 1, 1]
+-- NeuralNet n =
+
+sampleInput :: Vector R
+sampleInput = [1, 1, 1]
+
+-- llayers = [ Matrix [ [] [] [] ] }
+
+layer :: Layer
+layer = Layer{ weights = (3><3) [1, 0, 0,
+                                    0, 1, 0,
+                                    0, 0, 1
+                                   ],
+                  activation = id
+                }
+
+llayers :: [Layer]
+llayers = [ layer 
+          ]
+
+score1 = scoreNeuralNetwork sampleInput llayers
+
+
+layers2 = [layer, layer]
+score2 = scoreNeuralNetwork sampleInput layers2
