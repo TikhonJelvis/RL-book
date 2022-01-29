6f38b34d5db7cdd64526a520506942cdca1c1cdd
Paritosh Desai <paritosh.desai@gmail.com>
2022-01-28 17:50:36 -0800

    Adding a few new tests, variable length layers

--- haskell/src/RL/Approx/NeuralNet.hs
+++ haskell/src/RL/Approx/NeuralNet.hs
@@ -45,8 +45,10 @@ scoreLayer Layer { weights, activation } input = cmap activation ( tr' weights #
 
 -- Need to look at the foldr and needing to flip the order
 scoreNeuralNetwork :: Vector R -> [Layer] -> Vector R
--- scoreNeuralNetwork input lls = foldr (.) id (map (flip scoreLayer lls) input 
-scoreNeuralNetwork input lls = foldr (.) id (map scoreLayer lls) input 
+-- scoreNeuralNetwork input lls = foldr (.) id (map (flip scoreLayer lls) input
+-- we need to flip, since the natural composition order is R to L, but the
+-- composition we want for matrix-multiplication, it needs to be L to R
+scoreNeuralNetwork input lls = foldr (flip (.)) id (map scoreLayer lls) input 
 
 
 -- Create a simple test
@@ -58,12 +60,13 @@ sampleInput = [1, 1, 1]
 
 -- llayers = [ Matrix [ [] [] [] ] }
 
+-- Create an identity matrix, single layer, reproduces the input
 layer :: Layer
 layer = Layer{ weights = (3><3) [1, 0, 0,
-                                    0, 1, 0,
-                                    0, 0, 1
-                                   ],
-                  activation = id
+                                  0, 1, 0,
+                                  0, 0, 1
+                                ],
+                  activation = max 0 
                 }
 
 llayers :: [Layer]
@@ -75,3 +78,31 @@ score1 = scoreNeuralNetwork sampleInput llayers
 
 layers2 = [layer, layer]
 score2 = scoreNeuralNetwork sampleInput layers2
+
+-- Create an identity matrix, single layer, add a activation function
+
+layer2a :: Layer
+layer2a = Layer{ weights = (3><3) [1, 0, 0,
+                                     0, 1, 0,
+                                     0, 0, 1
+                                    ],
+                  activation = (2 *)
+                }
+layers2a = [layer2a, layer2a]
+score2a = scoreNeuralNetwork sampleInput layers2a
+
+-- Create another layer, which can be strung with the first layer, but without having same shape
+
+layer3x2 :: Layer
+
+layer3x2 = Layer{ weights = (3><2) [1, 0,
+                                    0, 1,
+                                    1, 1
+                                   ],
+                  activation = id
+                }
+
+layers3x3a3x2 :: [Layer]
+layers3x3a3x2 = [layer, layer3x2]
+
+score3x2a = scoreNeuralNetwork sampleInput layers3x3a3x2
