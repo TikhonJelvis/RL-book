54983a2143d4193c3fb00d4460d941b9cd40d23a
Ashwin Rao <ashwin.rao@gmail.com>
2022-04-10 07:17:05 -0700

    changed plotting so as to make curves distinguishable even in grayscale

--- rl/gen_utils/common_funcs.py
+++ rl/gen_utils/common_funcs.py
@@ -14,10 +14,10 @@ def get_unit_sigmoid_func(alpha: float) -> Callable[[float], float]:
 if __name__ == '__main__':
     from rl.gen_utils.plot_funcs import plot_list_of_curves
     alpha = [2.0, 1.0, 0.5]
-    colors = ["r", "b", "g"]
+    colors = ["r-", "b--", "g-."]
     labels = [(r"$\alpha$ = %.1f" % a) for a in alpha]
     logistics = [get_logistic_func(a) for a in alpha]
-    x_vals = np.arange(-3.0, 3.01, 0.01)
+    x_vals = np.arange(-3.0, 3.01, 0.05)
     y_vals = [f(x_vals) for f in logistics]
     plot_list_of_curves(
         [x_vals] * len(logistics),
@@ -28,7 +28,7 @@ if __name__ == '__main__':
     )
 
     alpha = [2.0, 1.0, 0.5]
-    colors = ["r", "b", "g"]
+    colors = ["r-", "b--", "g-."]
     labels = [(r"$\alpha$ = %.1f" % a) for a in alpha]
     unit_sigmoids = [get_unit_sigmoid_func(a) for a in alpha]
     x_vals = np.arange(0.0, 1.01, 0.01)
