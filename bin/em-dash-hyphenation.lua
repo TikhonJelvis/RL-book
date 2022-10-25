15d205d728ba9ebf70951a29d890acc7bdd6455b
Tikhon Jelvis <tikhon@jelv.is>
2022-10-25 15:52:13 -0700

    Fixed hyphenation issues from em-dashes
    TeX won't hyphenate words that are connected with `---` and no spaces (`like--this`), which was causing some lines in the generated PDF to go into the margin. To fix this, we can define and use `\emdash` instead of `---`:
    
    ``` latex
    \newcommand{\emdash}{\nobreak---\nobreak\hskip0pt}
    ```
    
    Instead of doing the substitution manually, I added a Pandoc filter that automatically replaces `---` and `—` with `\emdash ` when generating TeX. On a cursory inspection, this solves (most?) of the em-dash hyphenation issues without causing additional problems.

---
+++ bin/em-dash-hyphenation.lua
@@ -0,0 +1,12 @@
+-- Replace em-dashes with \emdash so that words before/after em-dashes
+-- can be hyphenated correctly
+if FORMAT:match 'latex' then
+   function Str(s)
+      local t = s.text
+      if string.find(t, '---') or string.find(t, '—') then
+         t = string.gsub(t, '---', '\\emdash ')
+         t = string.gsub(t, '—', '\\emdash ')
+         return pandoc.RawInline('tex', t)
+      end
+   end
+end
