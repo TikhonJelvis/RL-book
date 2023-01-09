1d39bec5106675ec5beecb4226483ab0d6ccc900
Tikhon Jelvis <tikhon@jelv.is>
2023-01-09 12:38:25 -0800

    Fixed error in chapter1/probability.py
    Addresses #298

--- rl/chapter1/probability.py
+++ rl/chapter1/probability.py
@@ -13,10 +13,11 @@ class Distribution(ABC, Generic[A]):
     def sample(self) -> A:
         pass
 
+
 class OldDie(Distribution):
     def __init__(self, sides):
         self.sides = sides
-        
+
     def __repr__(self):
         return f"Die(sides={self.sides})"
 
@@ -32,13 +33,16 @@ class OldDie(Distribution):
 
 six_sided = OldDie(6)
 
+
 def roll_dice():
     return six_sided.sample() + six_sided.sample()
+
 
 @dataclass(frozen=True)
 class Coin(Distribution[str]):
     def sample(self):
         return "heads" if random.random() < 0.5 else "tails"
+
 
 @dataclass(frozen=True)
 class Die(Distribution):
@@ -47,9 +51,9 @@ class Die(Distribution):
     def sample(self):
         return random.randint(1, self.sides)
 
+
 def expected_value(d: Distribution[float], n: int) -> float:
     return statistics.mean(d.sample() for _ in range(n))
 
-expected_value(Die(6), 100)
 
-expected_value(Coin(), 100)
+expected_value(Die(6), 100)
