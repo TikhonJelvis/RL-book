f2ee9211ac51382aa3f9e8a5e25344cdb34aceb6
raban <rabanspiegel@gmail.com>
2023-03-10 18:51:04 -0800

    setup

--- project/tac.py
+++ project/tac.py
@@ -5,6 +5,8 @@ from dataclasses import dataclass
 from typing import Mapping, Tuple
 
 from rl.distribution import Categorical
+from rl.markov_decision_process import FiniteMarkovDecisionProcess
+from rl.markov_process import FiniteMarkovProcess, FiniteMarkovRewardProcess
 
 
 @dataclass(frozen=True)
@@ -16,3 +18,25 @@ class TacState:
 PositionOnHandMapping = Mapping[
     TacState,
     Mapping[int, Categorical[Tuple[TacState, float]]]
+]
+
+
+class SimpleTacMDP(FiniteMarkovDecisionProcess[TacState, int]):
+
+    def __init__(
+            self,
+            position: int
+    ):
+        self.position: int = position
+        super().__init__(self.get_action_transition_reward_map())
+
+    def get_action_transition_reward_map(self) -> PositionOnHandMapping:
+
+
+if __name__ == '__main__':
+
+    tac_mdp: FiniteMarkovDecisionProcess[TacState, int] = SimpleTacMDP(0)
+
+    print("Tac MDP")
+    print("-------------")
+    print(tac_mdp)
