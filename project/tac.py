9a818da39eddb3075334de309030f70e570d5b70
raban <rabanspiegel@gmail.com>
2023-03-11 12:19:15 -0800

    1 player 2 cards

--- project/tac.py
+++ project/tac.py
@@ -12,33 +12,35 @@ from rl.markov_process import FiniteMarkovProcess, FiniteMarkovRewardProcess
 @dataclass(frozen=True)
 class TacState:
     position: int
-    cards_on_hand: list[int]
+    cards_on_hand: Tuple[int]
 
 
-def possible_cards_on_hand() -> List[int]:
-    return [1, 2, 3, 4]
+def possible_cards_on_hand() -> List[Tuple[int, int]]:
+    cards = [1, 2, 3, 4]
+    double_cards = [[card1, card2]
+                    for card1 in cards for card2 in cards if card1 != card2]
+    return double_cards
 
 
 def possible_states():
-    return [TacState(position, (card_on_hand, )) for position in range(0, 20) for card_on_hand in list(possible_cards_on_hand())]
+    return [TacState(position, (card1, card2)) for position in range(0, 20) for (card1, card2) in possible_cards_on_hand()]
 
 
 def initialize_transition_map():
 
-    d: Dict[TacState, Dict[int, Categorical[Tuple[TacState, float]]]] = {}
+    transition_map: Dict[TacState,
+                         Dict[int, Categorical[Tuple[TacState, float]]]] = {}
 
     for state in possible_states():
-        print("State: ", state)
         next_state = {}
         for card in state.cards_on_hand:
-            print("Card: ", card)
             new_cards_on_hand = [c for c in state.cards_on_hand if c != card]
+            new_position = (state.position + card) % 2
             next_state[card] = Categorical(
-                {(TacState(state.position+card, tuple(new_cards_on_hand)), 0): 1})
-            print("Next State: ", next_state[card])
-        d[state] = next_state
+                {(TacState(new_position, tuple(new_cards_on_hand)), 0): 1})
+        transition_map[state] = next_state
 
-    return d
+    return transition_map
 
 
 if __name__ == '__main__':
